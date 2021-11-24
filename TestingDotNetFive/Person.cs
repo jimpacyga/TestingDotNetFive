67ad2b2edbe89fd551af3557bae08b420b75e40a
jpacyga <jpacyga@uwm.com>
2021-11-24 11:04:40 -0500

    Added Person class and unit test project.

---
+++ TestingDotNetFive/Person.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TestingDotNetFive
+{
+    public class Person
+    {
+        public int Age { get; set; }
+
+        public string HairColor { get; set; } = null;
+
+        public string Name { get; set; } = null;
+
+        public int NumberOfArms { get; set; }
+    }
+}
